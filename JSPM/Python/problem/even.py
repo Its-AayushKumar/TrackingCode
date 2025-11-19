684339fbf11f705f93baa458a5c4e2a62dfc80a8
aayush666k <aayush666k@gmail.com>
2025-11-19 23:59:07 +0530

    python

---
+++ JSPM/Python/problem/even.py
@@ -0,0 +1,5 @@
+a = int(input("Enter a Number : "))
+if(a%2 == 0):
+    print(f"{a} is an even number")
+else :
+    print(f"{a} is not an even number")
---
+++ JSPM/Python/problem/number_comparison.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number a:"))
+b = int(input("Enter the Second number b:"))
+
+if (a > b):
+    print(f"{a} is greater than {b} ")
+else:
+    print(f"{b} is greater than {a} ")

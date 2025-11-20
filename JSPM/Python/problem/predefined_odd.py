6a41b5705f533e2446896ad8a7c765043e387a41
aayush666k <aayush666k@gmail.com>
2025-11-20 12:26:42 +0530

    Add dictionary creation, odd number check, and multiplication table scripts

---
+++ JSPM/Python/problem/predefined_odd.py
@@ -0,0 +1,5 @@
+a =5 
+if(a%2 != 0):
+    print(f"{a} is an odd number")
+else :
+    print(f"{a} is not an even number")
---
+++ JSPM/Python/problem/table.py
@@ -0,0 +1,3 @@
+num = int(input("The number whose multiplication table is required :"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num*i}")

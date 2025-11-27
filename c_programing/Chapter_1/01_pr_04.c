3ac6eebd9835cafac18eaa7c759dcb0ab7f9986b
aayush666k <aayush666k@gmail.com>
2025-11-27 19:21:56 +0530

    Add comment explaining alternative simple interest formula
    Added a comment in 01_pr_04.c to show an alternative way to calculate simple interest. No functional changes to the code.

--- c_programing/Chapter_1/01_pr_04.c
+++ c_programing/Chapter_1/01_pr_04.c
@@ -4,6 +4,7 @@ int main()
 {
     int principal = 123, rate = 5, years = 2;
     float simple_interest = (principal * rate * ((float)years)) / 100;  
+    //float simple_interest = (principal * rate * years / 100.0 (This is also works same);  
     printf("The value of simple interest id %f", simple_interest);     
     return 0;
 }

b7bdb19ce958fe151a2bd86ca05c7cab75809e83
aayush666k <aayush666k@gmail.com>
2025-11-27 13:35:50 +0530

    Add variable naming examples and comment out sample code
    Added examples of variable naming conventions in 01_01_first.c to illustrate case sensitivity and clarity. Commented out the sample code for adding two numbers in 01_02_variable.c for instructional purposes.

--- c_programing/Chapter_1/01_01_first.c
+++ c_programing/Chapter_1/01_01_first.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
-int main(){
+int main()
+{
+    int tom;   /*both tom and Tom are considered as 
+                different variable;*/
+    int Tom;
+    int error_code;  //defined variable should be clear   
     printf("Hello world!!");
     return 0;
 }
--- c_programing/Chapter_1/01_02_variable.c
+++ c_programing/Chapter_1/01_02_variable.c
@@ -15,13 +15,13 @@ int main()
     return 0;
 }
 
-// Try yourself -- create a program to add two numbers in c
-//#include <stdio.h>
-//int main()
-{
-    int a = 6;
-    int b = 9;
-    int sum = a + b;
-    printf("The sum of a + b is %d", sum);
-    return 0;
-}
+// // Try yourself -- create a program to add two numbers in c
+// #include <stdio.h>
+// int main()
+// {
+//     int a = 6;
+//     int b = 9;
+//     int sum = a + b;
+//     printf("The sum of a + b is %d", sum);
+//     return 0;
+// }

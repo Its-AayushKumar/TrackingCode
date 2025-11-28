2b3282b96246a418bcd313506a1f194bbb24d189
aayush666k <aayush666k@gmail.com>
2025-11-28 17:24:28 +0530

    Refactor type declaration and add arithmetic file
    Removed 02_01_instructions.c and replaced it with 02_01_type_declaration_instructions.c, updating the code to use float variables and calculations. Added a new placeholder file 02_02_arithmetic_instructions.c for future arithmetic instruction examples.

---
+++ c_programing/Chapter_2/02_01_type_declaration_instructions.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    // int a, b   , c = 4;
+    // a=b=c;
+    // printf("The value of a is %d\n", a);
+    // printf("The value of b is %d\n", b);
+    // printf("The value of c is %d\n", c);
+    float a = 1.1;
+    float b= a+6.9;
+    printf("The value of b is %f\n",b);
+
+    return 0;
+}                   

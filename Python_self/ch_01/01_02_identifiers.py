6a7a55338d8c6d08921dd8e2cf52f855f7509836
aayush666k <aayush666k@gmail.com>
2025-11-29 11:45:49 +0530

    Add Python basics: print, identifiers, datatypes
    Added three introductory Python scripts: one for printing 'Hello world', one demonstrating valid identifiers and Python keywords, and one showing how to find the datatype of various objects using type().

---
+++ Python_self/ch_01/01_02_identifiers.py
@@ -0,0 +1,14 @@
+Maths = 23
+phy01 = 45 
+_chem = 45 
+bcvS= 88
+avg = (Maths + phy01 + _chem + bcvS)/4
+print(avg)
+
+# The identifiers can start Capital , small letters or underscore 
+# It can not start with num and space and it also must not contain special char 
+
+import keyword
+print("keywords in python -->")
+print(keyword.kwlist)
+# This all are special keywrods in python 
---
+++ Python_self/ch_01/01_03_finding_datatype.py
@@ -0,0 +1,15 @@
+a = 34
+b = 6.9
+c = 3 + 4j
+d = 'str'
+e = [1,2,3,4,5]
+f = (1,2,3,4,5)
+g = {'fruit':'apple'}
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))

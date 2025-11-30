2163fc611beb7804de7056bf075da9ce19c45670
aayush666k <aayush666k@gmail.com>
2025-11-30 23:22:43 +0530

    Add solution for Chapter 2 exercise 4
    Added 02_pr_04.c with step-by-step evaluation of the expression 3*x/y-z+k, including variable initialization and output. Also added corresponding executables for exercises 3 and 4.

---
+++ c_programing/Chapter_2/02_pr_04.c
@@ -0,0 +1,14 @@
+/*explain step by step evaluation of 3*x/y-z+k
+where x=2 , y=3 , z=3 , k=1*/
+// 3*2/3-3+1 
+// 6/3-3+1
+// 2-3+1
+// -1+1
+// 0
+#include<stdio.h>
+
+int main(){
+    int x=2,y=3,z=3,k=1;
+    printf("The value of 3*x/y-z+k is %d \n",3*x/y-z+k );
+    return 0;
+}

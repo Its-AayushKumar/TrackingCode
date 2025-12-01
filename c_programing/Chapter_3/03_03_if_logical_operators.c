e910313baaaa3e544174e08f41bf1163afcc1bbf
aayush666k <aayush666k@gmail.com>
2025-12-01 17:40:54 +0530

    Add C examples for if-else, logical, ternary, switch
    Added new C source files demonstrating if-else, logical operators, else-if, ternary operator, and switch-case statements in Chapter 3. Corresponding compiled executables are also included for each example.

---
+++ c_programing/Chapter_3/03_03_if_logical_operators.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    int age ;
+    int vipPass = 1;
+    
+
+    printf("Enter your age :");
+    scanf("%d",&age);
+
+    if(age <=70 && age>=18 || (vipPass==1)){
+        printf("You are above 18 anf below 70,you can drive \n");
+    }
+    else{
+        printf("You cannot drive \n");
+    }
+    return 0;
+}

f7304b03982a4fce7aca117989d1a1e20a7a2c46
aayush666k <aayush666k@gmail.com>
2025-12-19 23:41:29 +0530

    minor updates

---
+++ c_programing/Chapter 8 stings/08_01_into_to_strings.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    // char str[] = {'A', 'a', 'y', 'u', 's', 'h', '\0'};
+
+    // '\0' is the null character to tell the string ends here
+
+    char str[] = 'Aayush'; // Here no need to put null character
+    printf(str);
+    return 0;
+}

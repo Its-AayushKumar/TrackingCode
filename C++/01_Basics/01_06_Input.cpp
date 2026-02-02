185d1d54214011167d43afa3e5b3f5e4c646ebe9
aayush666k <aayush666k@gmail.com>
2026-02-02 20:16:33 +0530

    Add C++ input/output example with error and log messages

---
+++ C++/01_Basics/01_06_Input.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    // Printing data
+    std::cout << "Hello c++23" << std::endl;
+
+    int age{21};
+    std::cout << "Age:" << age << std::endl;
+
+    std::cerr << "Error message : Something is wrong" << std::endl;
+    std::clog << "Log message : Something happened " << std::endl;
+
+    // Taking input from user
+    int age1;
+    std::string name;
+
+    std::cout << "Please type your name and age :" << std::endl;
+
+    std::cin >> name;
+    std::cin >> age1;
+
+    std::cout << "Hello" << name << "you are" << age1 << "years old" << std::endl;
+
+    return 0;
+}

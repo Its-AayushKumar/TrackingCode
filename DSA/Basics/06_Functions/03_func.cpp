284484bc40a81f368edd6aa78a9d8f33d10b4534
aayush666k <aayush666k@gmail.com>
2026-06-10 23:41:47 +0530

    Refactor code structure for improved readability and maintainability

---
+++ DSA/Basics/06_Functions/03_func.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+// pass by reference
+void dosomething(string &s)
+{
+    s[0] = 't';
+    cout << s << endl;
+}
+
+
+int main()
+{
+    string s = "raj";
+    dosomething(s);
+    cout << s << endl;
+
+    return 0;
+}
